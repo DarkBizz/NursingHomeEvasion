34b60cd5f84989494a6e88fa2466810e5ef2b86f
Bourhane <bossdbz@hotmail.com>
2014-07-11 17:55:19 +0200

    jeu gagné, rechargement de la scene

---
+++ Assets/Script/End Game/WonGame.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WonGame : MonoBehaviour {
+
+    public string sceneToLoad;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Senior"))
+        {
+            if (sceneToLoad != "")
+            {
+                Application.LoadLevel(sceneToLoad);
+            }
+            else
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+    }
+}
